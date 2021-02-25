bb2002d29955dd055a819cace7fbf5f2fd3ed07f
jcd1006 <70206527+jcd1006@users.noreply.github.com>
2021-02-25 08:37:55 -0500

    Add files via upload

--- baseballSign/src/baseballSign/FileManage.java
+++ baseballSign/src/baseballSign/FileManage.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 class FileCheck
 {
@@ -13,22 +15,22 @@ class FileCheck
 	{
 
 	}
-	
+
 	//check to see if the files exist that will be used for data storage and searching
 	public boolean doesExist(File userFile)
 	{
 		if(userFile.exists() == true)
 		{
-			
+
 			System.out.println(userFile.getName() + " exists");
 			return true;		
 		}
 		if(userFile.exists() == false)
 		{
-			
+
 			System.out.println(userFile.getName() + " does not exist");
 			System.out.println("Attempting to create file");
-			
+
 			try
 			{
 				if(userFile.createNewFile() == true)
@@ -37,27 +39,27 @@ class FileCheck
 					return true;
 				}
 			}
-			
+
 			catch(IOException errorName)
 			{
 				//if the file can't open, this prevents the whole program from crashing
 				System.out.println(userFile.getName() + " was not able to be created");
-				
+
 				//prints the details of the error through the exception handler  
 				errorName.printStackTrace();
 				return false;
 			}
-			
+
 			return true;
 		}
-		
+
 		else
 		{
 			System.out.println("Unable to determine file existance");
 			return false;
 		}
 	}
-	
+
 }
 
 //this class is used to determine which file user input goes
@@ -68,7 +70,7 @@ class Determine
 	String tempStr;
 	String checkVal;
 	int lineCount;
-	
+
 	//constructor
 	Determine()
 	{
@@ -77,30 +79,71 @@ class Determine
 		checkVal = " ";
 		lineCount = 0;
 	}
-	
+
 	//The user input will contain an int that tell us if the player stole or not
 	//That will be used to sort the input string into the correct file
-	public void sortInput(String userVal)
+	public void sortInput(File userFile, File newFile)
 	{
+		try 
+		{
+			if(newFile.length() >= 0)
+			{
+				newFile.delete();
+				newFile.createNewFile();
+			}
+			
+			Scanner sortSc = new Scanner(userFile);
+			FileWriter fileWrite = new FileWriter(newFile);
+			BufferedWriter buffWrite = new BufferedWriter(fileWrite);
+			String steal = String.valueOf("1");
+			
+			while(sortSc.hasNextLine() == true)
+			{
+				String fLine[] = sortSc.nextLine().split(" ", 2);
+				if(fLine[1].endsWith(steal))
+				{
+					buffWrite.write(fLine[0]);
+					buffWrite.newLine();
+				}
+			}
+			buffWrite.close();
+			sortSc.close();
+		}
+		
+		catch (FileNotFoundException e) 
+		{
+			
+			System.out.println("Scanner error: Sort method");
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			
+			System.out.println("FileWriter error: Sort method");
+			e.printStackTrace();
+		}
+		
 		
 	}
-	
+
 	//searches the file to find the steal signal
 	//will only be used on the steal file because we don't care about the sequences that didn't result in a steal
 	public void searchFile(File userFile)
 	{
+		System.out.println("Finding Steal Signal...");
+		
 		try
 		{   
 			//creates a file-reader of the class "BufferedReader" that is used to access the user file
 			BufferedReader readFile = new BufferedReader(new FileReader(userFile));
 			int fileLines = 0;
-			
+
 			//as long as the file has a valid next line, increase fileLines
 			while(readFile.readLine() != null)
 			{
 				fileLines++;
 			}
-			
+
 				for(int i = 0; i < fileStr.length(); i++)
 				{
 					Scanner sc = new Scanner(userFile);
@@ -114,21 +157,21 @@ class Determine
 						tempStr = sc.nextLine();
 						//checks for a pair of characters
 						checkVal = fileStr.substring(i, i+2);
-						System.out.println("Checking for sequence: " + checkVal);
+//						System.out.println("Checking for sequence: " + checkVal);
 						lineCount = lineCount +1;
-					
+						
 						//checks to make sure we don't try to access outside of the string length
-						if((i+2)>tempStr.length())
+						if((i+1)>=fileStr.length())
 						{
 							System.out.println("End of string");
 							break;
 						}
-					
+
 						//checks if each line contains the substring
 						if(tempStr.contains(checkVal) == true)
 						{
-							System.out.println("Sequence found in line: " + lineCount);
-							
+//							System.out.println("Sequence found in line: " + lineCount);
+
 							//this lets us know when the end of the file has been reached
 							if(lineCount + 1 == fileLines)
 							{
@@ -140,13 +183,13 @@ class Determine
 						else
 						{
 							//alerts that the sequence was not found in the string and resets the search
-							System.out.println("Sequence not found int line: " + lineCount);
+//							System.out.println("Sequence not found int line: " + lineCount);
 							lineCount = 0;
 							break;
 						}
 					}
 					sc.close();
-					
+
 					//has to have the "+1" because we take the first string away and use it to search
 					//therefore lineCount will never equal fileLines and we need the added one
 					if(lineCount +1 == fileLines)
@@ -154,7 +197,7 @@ class Determine
 						break;
 					}
 				}
-			
+
 			readFile.close();
 		}
 		//error handling for Scanner opening
@@ -162,36 +205,40 @@ class Determine
 		{
 			e.printStackTrace();
 		}
-		
+
 		//error handling for BufferedReader opening
 		//Java likes to throw errors without these catch statements
 		catch (IOException e1)
 		{
-		
+
 			e1.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
 
 
 
 public class FileManage {
-	
+
 	public static void main(String args[])
 	{
-		File stlFile = new File("SecondStealFile.txt");
+		File allData = new File("AllData2.txt");
+		File stlFile = new File("StealFile.txt");
 		File noStlFile = new File("NoStealFile.txt");
-		
+
 		FileCheck check = new FileCheck();
 		Determine find = new Determine();
-		
+
+		check.doesExist(allData);
 		check.doesExist(stlFile);
 		check.doesExist(noStlFile);
 		
+		find.sortInput(allData, stlFile);
+
 		find.searchFile(stlFile);
-		
+
 		System.out.println("END OF TEST");
 	}
 
